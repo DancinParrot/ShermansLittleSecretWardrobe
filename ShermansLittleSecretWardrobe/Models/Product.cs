cde7b68958222e8458157e8f00cb0d75fb5b7385
Kai Zhe Tan <s10219390@connect.np.edu.sg>
2022-07-02 01:29:10 +0800

    Added Product and Image models.

---
+++ ShermansLittleSecretWardrobe/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShermansLittleSecretWardrobe.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = String.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
