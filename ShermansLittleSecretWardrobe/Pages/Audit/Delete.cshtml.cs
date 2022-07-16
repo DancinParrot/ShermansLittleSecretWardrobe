f3cb7570c355bcc2d3f19a15d28849428b81f9c5
Ng E Kai <s10223145@connect.np.edu.sg>
2022-07-16 10:21:46 +0800

    Registration + login + audit page

---
+++ ShermansLittleSecretWardrobe/Pages/Audit/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ShermansLittleSecretWardrobe.Data;
+using ShermansLittleSecretWardrobe.Models;
+
+namespace ShermansLittleSecretWardrobe.Pages.Audit
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ShermansLittleSecretWardrobe.Data.ShermansLittleSecretWardrobeContext _context;
+
+        public DeleteModel(ShermansLittleSecretWardrobe.Data.ShermansLittleSecretWardrobeContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public AuditRecord AuditRecord { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.AuditRecords == null)
+            {
+                return NotFound();
+            }
+
+            var auditrecord = await _context.AuditRecords.FirstOrDefaultAsync(m => m.Audit_ID == id);
+
+            if (auditrecord == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                AuditRecord = auditrecord;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.AuditRecords == null)
+            {
+                return NotFound();
+            }
+            var auditrecord = await _context.AuditRecords.FindAsync(id);
+
+            if (auditrecord != null)
+            {
+                AuditRecord = auditrecord;
+                _context.AuditRecords.Remove(AuditRecord);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
