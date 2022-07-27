f8953e784c1cd80a1183e28fa24142ae6542a24c
Sherman <shermanping@gmail.com>
2022-07-27 23:47:12 +0800

    Updated to Homepage

---
+++ ShermansLittleSecretWardrobe/Pages/Feedback.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShermansLittleSecretWardrobe.Pages
+{
+    public class FeedbackModel : PageModel
+    {
+        private readonly ILogger<FeedbackModel> _logger;
+
+        public FeedbackModel(ILogger<FeedbackModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
